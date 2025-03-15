e8baa30fa104969a059af00f42c213f3671f7467
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-03-15 15:01:59 +0530

    auth middleware complete

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controller/user.controller');
-
+const Auth = require('../middleware/Auth');
 userRouter.post('/register', userController.signUpUser);
 userRouter.post('/login', userController.signInUser)
 
 userRouter.get('/verify', userController.verify)
+
 module.exports = userRouter;

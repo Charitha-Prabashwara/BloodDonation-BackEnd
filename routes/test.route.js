9d6ed0abb33e5c0b01b6f83de7675db7975bbbda
Charitha-Prabashwara <prabhashwara.seu@gmail.com>
2025-10-04 13:24:16 +0530

    docker complete

--- routes/test.route.js
+++ routes/test.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const testRouter = express.Router();
 
 
-testRouter.post('/', (req, res)=>{
+testRouter.get('/', (req, res)=>{
     return res.status(200).json({
         success: true,
         message: 'working'
